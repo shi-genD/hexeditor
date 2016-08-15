b2a0c9d43102f40406083e911c515d88d972c151
shi <malishevsky.al@gmail.com>
2016-08-15 13:48:56 +0300

    added based classes

---
+++ src/HexValidator.java
@@ -0,0 +1,15 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by shi on 15.08.16.
+ */
+public class HexValidator {
+    private static Pattern pat = Pattern.compile("[A-Fa-f0-9][A-Fa-f0-9]");
+
+    public static boolean isValid(String hexByte) {
+        Matcher mat;
+        mat = pat.matcher(hexByte);
+        return mat.matches();
+    }
+}
---
+++ src/IncorrectInputCodeException.java
@@ -0,0 +1,12 @@
+/**
+ * Created by shi on 15.08.16.
+ */
+public class IncorrectInputCodeException extends Exception {
+    IncorrectInputCodeException() {
+        super();
+    }
+
+    IncorrectInputCodeException(String str) {
+        super(str);
+    }
+}
